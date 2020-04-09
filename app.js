bde6301c911b0e821728d9d57d4a2b5e63328136
marshal <marshal.wu@qq.com>
2020-04-09 16:20:54 +0800

    支持基于json的列表页并实现cache功能

--- app.js
+++ app.js
@@ -4,26 +4,42 @@ import logger from './lib/logger'
 import exitHandler from './lib/exit-handler'
 
 import { crawl } from './lib/spider'
+import extractUrls from './lib/json-extractor'
 
-let configFilePath = './config/spider.yml'
+(async function () {
+    let configFilePath = './config/spider.yml'
 
-const args = process.argv.slice(2)
-if (args.length > 0) {
-    configFilePath = args[0]
-}
+    // 获取命令行参数的配置文件路径，如果有的话
+    const args = process.argv.slice(2)
+    if (args.length > 0) {
+        configFilePath = args[0]
+    }
 
-const config = yaml.load(fs.readFileSync(configFilePath, 'utf8'))
-logger.level = config.logging.level
+    const config = yaml.load(fs.readFileSync(configFilePath, 'utf8'))
+    logger.level = config.logging.level
 
-crawl(config).then(() => {
-    logger.info({
-        status: 'Spider end.'
+    // 前置处理，针对列表页是json的情况
+    let { jsonUrls, jsonPath } = config
+    if (jsonUrls != null) {
+        let articleUrls = []
+        for (let url of jsonUrls) {
+            articleUrls = [].concat(await extractUrls(url, jsonPath), articleUrls)
+        }
+        config.urls = articleUrls
+    }
+
+    // 开始传统的爬虫过程
+    crawl(config).then(() => {
+        logger.info({
+            status: 'Spider end.'
+        })
+        process.exit(0)
     })
-    process.exit(0)
-})
 
-logger.info({
-    status: 'Spider started.'
-})
+    logger.info({
+        status: 'Spider started.'
+    })
 
-exitHandler()
+    exitHandler()
+
+}())
